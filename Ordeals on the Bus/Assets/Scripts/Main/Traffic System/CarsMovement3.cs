7af475aae77fe9c8f0a8f3417aba1da22a9574fe
Tearai <77899947+Tearai@users.noreply.github.com>
2024-04-23 15:33:14 +0100

    more cars

---
+++ Ordeals on the Bus/Assets/Scripts/Main/Traffic System/CarsMovement3.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarsMovement3 : MonoBehaviour
+{
+    public float speed;
+    public bool lane1;
+    public bool lane2;
+
+    public float _crashedState;
+
+    public float _durationOfCrash = 1.0f;
+
+    private bool isCrashing = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lane1 == true)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+
+        if (lane2 == true)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            speed = 0;
+            if (!isCrashing)
+            {
+                isCrashing = true;
+                StartCoroutine(CrashAnimation());
+            }
+        }
+    }
+
+    IEnumerator CrashAnimation()
+    {
+        float timer = 0f;
+        Vector3 originalScale = transform.localScale;
+        Vector3 targetScale = new Vector3(transform.localScale.x, transform.localScale.y, _crashedState);
+
+        while (timer < _durationOfCrash)
+        {
+            transform.localScale = Vector3.Lerp(originalScale, targetScale, timer / _durationOfCrash);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = targetScale; // Ensure we reach the exact scale at the end
+    }
+}
